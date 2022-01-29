f1dc48faf826140a7c25262c49c08f575a1ca72b
Akash <akashu912@gmail.com>
2022-01-29 22:41:41 +0530

    Updates

---
+++ precache-manifest.f58118c15b8d5a4aa3fb25fcd9e07d41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b269187291ba00dad86fb37d55d9fa3",
+    "url": "/akash-portfolio/index.html"
+  },
+  {
+    "revision": "9315649f468826fe59e7",
+    "url": "/akash-portfolio/static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "895b8cdbe66e0e33ceab",
+    "url": "/akash-portfolio/static/css/main.715aad19.chunk.css"
+  },
+  {
+    "revision": "9315649f468826fe59e7",
+    "url": "/akash-portfolio/static/js/2.1958f00f.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/akash-portfolio/static/js/2.1958f00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895b8cdbe66e0e33ceab",
+    "url": "/akash-portfolio/static/js/main.f73ad9f6.chunk.js"
+  },
+  {
+    "revision": "eb927f7b40a6eda0afa8",
+    "url": "/akash-portfolio/static/js/runtime-main.1a8f8478.js"
+  }
+]);
